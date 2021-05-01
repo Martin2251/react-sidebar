05431fa0dfdcb84600126a088f106a8592271e56
martin lowe <martinlowe673@gmail.com>
2021-05-01 14:12:44 +0100

    added icons

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+  <div className="navbar">
+  <Link to="#" className='menubars'>
+
+  </Link>
+  </div>
+    </>
+  )
+}
+
+export default Navbar
